d811ae76e4529131992cd7f01f2c5e5cd39321ae
pasqualedevita <>
2021-12-02 13:04:20 +0100

    env

--- azure-devops/app-projects/00_secrets.tf
+++ azure-devops/app-projects/00_secrets.tf
@@ -5,9 +5,7 @@ module "secrets" {
   key_vault_name = local.prod_key_vault_name
 
   secrets = [
-    "DANGER-GITHUB-API-TOKEN",
     "azure-devops-github-ro-TOKEN",
-    "azure-devops-github-rw-TOKEN",
     "azure-devops-github-pr-TOKEN",
     "azure-devops-github-EMAIL",
     "azure-devops-github-USERNAME",
@@ -17,14 +15,44 @@ module "secrets" {
     "DEV-SUBSCRIPTION-ID",
     "UAT-SUBSCRIPTION-ID",
     "PROD-SUBSCRIPTION-ID",
-    "dev-usrreg-aks-apiserver-url",
-    "dev-usrreg-aks-azure-devops-sa-token",
-    "dev-usrreg-aks-azure-devops-sa-cacrt",
-    # "uat-usrreg-aks-apiserver-url",
-    # "uat-usrreg-aks-azure-devops-sa-token",
-    # "uat-usrreg-aks-azure-devops-sa-cacrt",
-    # "prod-usrreg-aks-apiserver-url",
-    # "prod-usrreg-aks-azure-devops-sa-token",
-    # "prod-usrreg-aks-azure-devops-sa-cacrt"
+  ]
+}
+
+module "secrets_dev" {
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v1.0.11"
+
+  resource_group = local.dev_key_vault_resource_group
+  key_vault_name = local.dev_key_vault_name
+
+  secrets = [
+    "aks-apiserver-url",
+    "aks-azure-devops-sa-cacrt",
+    "aks-azure-devops-sa-token",
+  ]
+}
+
+module "secrets_uat" {
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v1.0.11"
+
+  resource_group = local.uat_key_vault_resource_group
+  key_vault_name = local.uat_key_vault_name
+
+  secrets = [
+    "aks-apiserver-url",
+    "aks-azure-devops-sa-cacrt",
+    "aks-azure-devops-sa-token",
+  ]
+}
+
+module "secrets_prod" {
+  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v1.0.11"
+
+  resource_group = local.prod_key_vault_resource_group
+  key_vault_name = local.prod_key_vault_name
+
+  secrets = [
+    "aks-apiserver-url",
+    "aks-azure-devops-sa-cacrt",
+    "aks-azure-devops-sa-token",
   ]
 }
--- azure-devops/app-projects/variables.tf
+++ azure-devops/app-projects/variables.tf
@@ -23,13 +23,13 @@ locals {
   prefix           = "usrreg"
   azure_devops_org = "pagopaspa"
 
-  dev_key_vault_name  = format("%s-d-kv", local.prefix)
-  uat_key_vault_name  = format("%s-u-kv", local.prefix)
-  prod_key_vault_name = format("%s-p-kv", local.prefix)
+  dev_key_vault_name  = format("%s-d-kv-neu", local.prefix)
+  uat_key_vault_name  = format("%s-u-kv-neu", local.prefix)
+  prod_key_vault_name = format("%s-p-kv-weu", local.prefix)
 
-  dev_key_vault_resource_group  = format("%s-d-sec-rg", local.prefix)
-  uat_key_vault_resource_group  = format("%s-u-sec-rg", local.prefix)
-  prod_key_vault_resource_group = format("%s-p-sec-rg", local.prefix)
+  dev_key_vault_resource_group  = format("%s-d-sec-rg-neu", local.prefix)
+  uat_key_vault_resource_group  = format("%s-u-sec-rg-neu", local.prefix)
+  prod_key_vault_resource_group = format("%s-p-sec-rg-weu", local.prefix)
 
   dev_vnet_rg  = format("%s-d-vnet-rg", local.prefix)
   uat_vnet_rg  = format("%s-u-vnet-rg", local.prefix)

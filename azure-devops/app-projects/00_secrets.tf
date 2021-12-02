9057734e968e449a9130386e05518a411e265a4d
pasqualedevita <>
2021-12-02 13:09:18 +0100

    only_pord

--- azure-devops/app-projects/00_secrets.tf
+++ azure-devops/app-projects/00_secrets.tf
@@ -31,28 +31,28 @@ module "secrets_dev" {
   ]
 }
 
-module "secrets_uat" {
-  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v1.0.11"
+# module "secrets_uat" {
+#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v1.0.11"
 
-  resource_group = local.uat_key_vault_resource_group
-  key_vault_name = local.uat_key_vault_name
+#   resource_group = local.uat_key_vault_resource_group
+#   key_vault_name = local.uat_key_vault_name
 
-  secrets = [
-    "aks-apiserver-url",
-    "aks-azure-devops-sa-cacrt",
-    "aks-azure-devops-sa-token",
-  ]
-}
+#   secrets = [
+#     "aks-apiserver-url",
+#     "aks-azure-devops-sa-cacrt",
+#     "aks-azure-devops-sa-token",
+#   ]
+# }
 
-module "secrets_prod" {
-  source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v1.0.11"
+# module "secrets_prod" {
+#   source = "git::https://github.com/pagopa/azurerm.git//key_vault_secrets_query?ref=v1.0.11"
 
-  resource_group = local.prod_key_vault_resource_group
-  key_vault_name = local.prod_key_vault_name
+#   resource_group = local.prod_key_vault_resource_group
+#   key_vault_name = local.prod_key_vault_name
 
-  secrets = [
-    "aks-apiserver-url",
-    "aks-azure-devops-sa-cacrt",
-    "aks-azure-devops-sa-token",
-  ]
-}
+#   secrets = [
+#     "aks-apiserver-url",
+#     "aks-azure-devops-sa-cacrt",
+#     "aks-azure-devops-sa-token",
+#   ]
+# }
